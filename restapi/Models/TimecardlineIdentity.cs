9d1a544563b844e5e0aa2959726acea3cbb5342f
KattyZhao <zhaos@seattleu.edu>
2019-01-17 13:38:29 -0800

    Add files via upload

---
+++ restapi/Models/TimecardlineIdentity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace restapi.Models
+{
+    public class TimecardlineIdentity
+    {
+        public TimecardlineIdentity(string area, string number)
+        {
+            this.Area = area;
+            this.Number = number;
+        }
+
+        public TimecardlineIdentity(string number) : this("line", number) { }
+
+        public TimecardlineIdentity() : this(GetSerialNumber().ToString()) { }
+
+        //public string Area { get; set; }
+
+        //public string Number { get; set; }
+
+        public string Value { get => $"{Area}-{Number}"; }
+
+        private static long GetSerialNumber()
+        {
+            return new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+        }
+    }
+}
+
